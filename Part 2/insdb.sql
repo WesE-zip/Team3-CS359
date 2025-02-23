82f7ababf76af6a081467be47d0850260b3592a2
krispkreme <96608792+krispkreme@users.noreply.github.com>
2025-02-23 16:46:53 -0700

    Update insdb.sql, added member, membershipPlan, payment

--- Part 2/insdb.sql
+++ Part 2/insdb.sql
@@ -1,4 +1,4 @@
--- Cruz Urbina
+-- Cruz Urbina, Christian Kurdi
 -- Database Design
 
 -- INSERT TEMP DATA
@@ -7,11 +7,34 @@
 -- INSERT INTO class (proxy) VALUES (1);
 -- INSERT INTO class (proxy) VALUES (1);
 -- INSERT INTO class (proxy) VALUES (1);
--- INSERT INTO member (proxy) VALUES (1);
--- INSERT INTO member (proxy) VALUES (1);
--- INSERT INTO member (proxy) VALUES (1);
--- INSERT INTO member (proxy) VALUES (1);
--- INSERT INTO member (proxy) VALUES (1);
+
+--INSERT MEMBER DATA
+INSERT INTO member(memberId, name, email, phone, address, age, membershipStartDate, membershipEndDate)
+VALUES 
+  (1, 'Dwight Schrute', 'beetsmotel@dundermifflin.com', '(888)666-7986', '1212 Livin St Scranton, PA 18447', 35, '2025-01-01', '2025-02-01'),
+  (2, 'Michael Scott', 'thatswhat@dundermifflin.com', '(888)452-6985', '3505 Bad Call Dr Scranton, PA 18447', 40, '2025-01-01', '2026-02-01'),
+  (3, 'Jim Halpert', 'receptionlover@dundermifflin.com', '(888)892-4562', '7878 Parents Pl Scranton, PA 18447', 30, '2025-03-15', '2026-03-15'),
+  (4, 'Pam Beasley', 'saleslover@dundermifflin.com', '(888)651-8889', '7878 Parents Pl Scranton, PA 18447', 29, '2025-03-15', '2026-03-15'),
+  (5, 'Kevin Malone', 'barkingdogs@dundermifflin.com', '(888)959-6454', '1111 Chili Way Scranton, PA 18447', 40, '2025-01-01', '2026-01-01');
+
+--INSERT MEMBERSHIP PLAN DATA
+INSERT INTO membershipPlan(planId, planType, cost)
+VALUES 
+  (1, 'Monthly', 49.95),
+  (2, 'Monthly', 69.95),
+  (3, 'Annually', 99.99), 
+  (4, 'Annually', 125.99),
+  (5, 'Annually', 200.99);
+
+--INSERT PAYMENT DATA
+INSERT INTO payment(paymentId, memberId, planId, amountPaid, paymentDate)
+VALUES
+  (1, 1, 1, 49.95, '2025-01-01'),
+  (2, 1, 2, 69.95, '2025-01-20'),
+  (3, 2, 2, 69.95, '2025-01-01'),
+  (4, 4, 3, 99.99, '2025-03-15'),
+  (5, 5, 5, 200.99, '2025-01-01');
+
 
 -- INSERT GYM FACILITY DATA
 INSERT INTO gym_facility (location, phone, manager)
